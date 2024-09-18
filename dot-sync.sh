faf3bf9c25ea9c3df1a9c058689a54071bc9c377
booth-w <personal@booth-w.co.uk>
2024-09-18 19:29:02 +0100

    Added dot-sync script

---
+++ dot-sync.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+if [ ! -d ~/projects/dotfiles ]; then
+	mkdir -p ~/projects
+	git clone https://github.com/booth-w/dotfiles.git ~/projects/dotfiles
+fi
+
+cd ~/projects/dotfiles
+git pull
+
+cp .bashrc ~/.bashrc
+cp -r awesome ~/.config/
+cp -r fastfetch ~/.config/
+cp -r kitty ~/.config/
+cp -r rofi ~/.config/
+cp -r superfile ~/.config/
+cp -r VSCodium ~/.config/

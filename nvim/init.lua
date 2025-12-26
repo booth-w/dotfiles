293efbe30302ad5398f5f88787a5084d46e9610b
booth-w <personal@booth-w.co.uk>
2025-12-26 15:14:43 +0000

    Remove telescopt padding

--- nvim/init.lua
+++ nvim/init.lua
@@ -250,6 +250,16 @@ local ts_actions = require("telescope.actions")
 local fb_actions = require("telescope").extensions.file_browser.actions
 require("telescope").setup({
 	defaults = {
+		layout_config = {
+			horizontal = {
+				width = {
+					padding = 0
+				},
+				height = {
+					padding = 0
+				}
+			}
+		},
 		mappings = {
 			["n"] = {
 				["<C-c>"] = "close",
